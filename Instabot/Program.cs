16b66713808c9404cbea1313f5184e15048e9e8c
Filip Plass <u18.422@pslib.cloud>
2021-08-15 23:07:20 +0200

    Send 5 memes, img or carousel only

--- Instabot/Program.cs
+++ Instabot/Program.cs
@@ -4,6 +4,8 @@ using InstaSharper.Classes;
 using InstaSharper.Classes.Models;
 using InstaSharper.Logger;
 using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Instabot
 {
@@ -11,8 +13,11 @@ namespace Instabot
     {
         private static UserSessionData user;
         private static IInstaApi api;
+
+
         static void Main(string[] args)
         {
+            
             user = new UserSessionData();
             user.UserName = "kocour_endorfin"; //User name
             user.Password = "Falco77!"; //password
@@ -34,7 +39,7 @@ namespace Instabot
             if (loginRequest.Succeeded)
             {
                 Console.WriteLine("LOGGED IN!");
-                SendMessage("kucerova_v"); //měníš tady
+                SendMessage("total_filin"); //měníš tady
             }
             else
             {
@@ -43,11 +48,63 @@ namespace Instabot
         }
         public static async void SendMessage(string recipient)
         {
-            IResult<InstaUser> userToContact = await api.GetUserAsync(recipient);
-
-            Console.WriteLine("User: " + userToContact.Value.FullName);
-            IResult<InstaDirectInboxThreadList> mess = await api.SendDirectMessage(userToContact.Value.Pk.ToString(), string.Empty, "Ahoj prdelko, jsem robot");
+            IResult<InstaUser> userToContact = await api.GetUserAsync(recipient); //samostatný příjemce
+            InstaRankedRecipientThread threadik = new InstaRankedRecipientThread(); //thread příjemce s userem
+            var usersFollowers = await api.GetUserFollowersAsync(user.UserName, PaginationParameters.MaxPagesToLoad(5)); // seznam followerů
+            var recipientsResult = await api.GetRankedRecipientsAsync(); //seznam chatů
+            if (!recipientsResult.Succeeded)
+            {
+                Console.WriteLine("Unable to get ranked recipients");
+                return;
+            }
+            foreach (var thread in recipientsResult.Value.Threads)
+            {
+                
+                if (thread.ThreadTitle == recipient)
+                {
+                    threadik = thread;
+                }
+            }
+            
+           var feed = await api.GetTagFeedAsync("meme", PaginationParameters.MaxPagesToLoad(1)); //seznam příspěvků podle hashtagu
+            var feedToSend = feed.Value.Medias.Where(x=>x.MediaType == InstaMediaType.Image || x.MediaType == InstaMediaType.Carousel).Take(6); // pouze 5
+            foreach (var i in feedToSend)
+            {
+                var result =
+                    await api.ShareMedia(i.Caption.MediaId, i.MediaType, threadik.ThreadId);
+            }
+            var inboxThreads = await api.GetDirectInboxAsync();
+            if (!inboxThreads.Succeeded)
+            {
+                Console.WriteLine("Unable to get inbox");
+                return;
+            }
+            //
             
         }
+       
     }
 }
+
+
+
+
+//Console.WriteLine("User: " + userToContact.Value.FullName);
+//IResult<InstaDirectInboxThreadList> mess = await api.SendDirectMessage(userToContact.Value.Pk.ToString(), string.Empty, "Cau vole");
+/*var recipientsResult = await api.GetRankedRecipientsAsync(); //seznam chatů
+if (!recipientsResult.Succeeded)
+{
+    Console.WriteLine("Unable to get ranked recipients");
+    return;
+}
+Console.WriteLine($"Got {recipientsResult.Value.Threads.Count} ranked threads");
+foreach (var thread in recipientsResult.Value.Threads)
+{
+    Console.WriteLine($"Threadname: {thread.ThreadTitle}, users: {thread.Users.Count}");
+    if(thread.ThreadTitle == recipient)
+    {
+        IResult<InstaDirectInboxThreadList> mess = await api.SendDirectMessage(userToContact.Value.Pk.ToString(), string.Empty, "Cau vole");
+        var result =
+        await api.ShareMedia("1866111698328767752_3255807", InstaMediaType.Image, thread.ThreadId);
+    }
+}*/
